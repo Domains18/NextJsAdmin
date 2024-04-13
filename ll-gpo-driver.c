7acd47815c3a4555353aa62431eeb25caaadbcb6
Domains18 <gibsonsgibson88@gmail.com>
2024-04-13 21:32:44 +0300

    ll-gpo-driver

--- ll-gpo-driver.c
+++ ll-gpo-driver.c
@@ -1,13 +1,17 @@
 #include <linux/kernel.h>
-#include <linuc/init.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+#include <linux/proc_fs.h>
 #include <linux/slab.h>
+
 #include <asm/io.h>
 
-#define LLL_MAX_USER_SIE 1024
-
+#define LLL_MAX_USER_SIZE 1024
 
 #define BCM2837_GPIO_ADDRESS 0x3F200000
 #define BCM2711_GPIO_ADDRESS 0xfe200000
+
 
 static struct proc_dir_entry *lll_proc = NULL;
 
@@ -32,5 +36,26 @@ static void gpio_pin_off(unsigned int pin){
 }
 
 ssize_t lll_read(struct  file *file, char __user *user, size_t size, loff_t *off){
+    return copy_to_user(user, "hello!\n", 7) ? 0 : 7;
+}
 
+ssize_t lll_write(struct file *file, char __user *user, size_t size, loff_t *off){
+    unsigned int pin = UINT_MAX;
+    unsigned int value = UINT_max;
+
+    memset(data_buffer, 0x0, sizeof(data_buffer));
+    if(size > LLL_MAX_USER_SIZE){
+        size = LLL_MAX_USER_SIZE
+    }
+    if(copy_from_user(data_buffer, user, size))
+        return 0;
+    printk("Data buffer: %s\n", data_buffer);
+    if(sscanf(data_buffer, "%d,%d", &pin, &value) != 2){
+        printk("improper data format submitted\n");
+        return size;
+    }
+    if(pin > 21 || pin < 0){
+        printk("invalid pin number submitted\n");
+        return size;
+    }
 }
